764d2f343a2c556ef3f5b865c0abe02699036d31
Alexander Leschanz <80907917+Creative-Difficulty@users.noreply.github.com>
2026-07-17 15:29:05 -0700

    Initial Commit

---
+++ ChitChat/ChitChatApp.swift
@@ -0,0 +1,32 @@
+//
+//  ChitChatApp.swift
+//  ChitChat
+//
+//  Created by Alexander Leschanz on 17.07.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ChitChatApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
